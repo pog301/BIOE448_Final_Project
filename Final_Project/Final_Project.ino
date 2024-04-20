693a361f5ad076b71888312beb73c95824f71619
pog301 <po8@rice.edu>
2024-04-19 22:26:47 -0500

    This should be the version of Final_Project sent to my email at 3:49pm on 4/19/24.

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,21 +1,21 @@
-// Code for Final Project for BIOE 448 
-// Citations at the end
+// Code for Final Project for BIOE 448 //ADDED
+// Citations at the end //ADDED
 
-#include <Servo.h>
+#include <Servo.h> //ADDED
 // #include "thingProperties.h"
 
-Servo myservo;  // Comment taken from Servo code: creates Servo object to control servo 1
+Servo myservo;  //ADDED // Comment taken from Servo code: creates Servo object to control servo 1
 //Servo myservo2; // Comment taken from Servo code: creates Servo object to control servo 2
 //Servo myservo3; // Comment taken from Servo code: creates Servo object to control servo 3
 const int ECG1_pin = A6; // Creates a constant integer with the signal input for the ECG sensor
 int pulse_signal = 0;
-int servo_value = 0; 
+int servo_value = 0; //ADDED
 int threshold = 30; // change! 
 char Str6[15] = "start"; 
 
 void setup() {
-  Serial.begin(9600);
-  myservo.attach(6);  // Comment taken from Servo code: Attaches the servo on pin 6 (PWM) to the Servo object
+  Serial.begin(9600); //ADDED
+  myservo.attach(6);//ADDED  // Comment taken from Servo code: Attaches the servo on pin 6 (PWM) to the Servo object
   //myservo2.attach(7); // Comment taken from Servo code: Attaches the servo on pin 7 (PWM) to the Servo object
   //myservo3.attach(8); // Comment taken from Servo code: Attaches the servo on pin 8 (PWM) to the Servo object
   
@@ -44,8 +44,8 @@ void loop() {
     servo_value = 180;
   }
   Serial.print(servo_value);
-  myservo.write(servo_value); 
-  delay(10);
+  myservo.write(servo_value); //ADDED
+  delay(10); //ADDED
   //myservo2.write(servo_value); 
   //delay(10);
   //myservo3.write(servo_value);
